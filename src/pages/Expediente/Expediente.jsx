f5739da11d3b957b7d10b2413c28504abc58a40c
Esteban Donis <estebandonis10@gmail.com>
2023-04-08 16:49:36 -0600

    Agregue Login

--- src/pages/Expediente/Expediente.jsx
+++ src/pages/Expediente/Expediente.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import Axios from 'axios'
 import PropTypes from 'prop-types'
-import { ChangeEvent, useEffect, useState } from 'react'
+import { useState } from 'react'
 import { Navbar, ShowAll } from '../../components'
 
-import { styles } from './Expediente.module.css'
+import { styles, inputs, bloques } from './Expediente.module.css'
 
 const Expediente = () => {
   
   const [responseData, setResponseData] = useState(null)
+  const [responseData1, setResponseData1] = useState(null)
   const [inputText, setInputText] = useState("");
   const [show, setShow] = useState(false)
 
@@ -19,7 +20,23 @@ const Expediente = () => {
 
   const handleClick = async() => {
     await loadMedicoByNum()
+    await loadMedicos()
+    console.log(responseData1)
     setShow(true)
+  }
+
+  const getMedicos = async() => {
+    try {
+      const response = await Axios.get("http://localhost:3000/api/v1/medicos/")
+      console.log(response.data)
+      return response.data
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  const loadMedicos = async () => {
+    setResponseData1(await getMedicos())
   }
 
   const getMedicoByNum = async() => {
@@ -38,13 +55,22 @@ const Expediente = () => {
   return (
     <div className={styles}>
       <Navbar />
-      <input type="text" onChange={handleChange} />
-      <button onClick={handleClick}>Submit</button>
-      {
-        show?
-        <ShowAll json={responseData}/>
-        :null
-      }
+      <div className={inputs}>
+        <input type="text" onChange={handleChange} placeholder='Ingrese el dpi del paciente'/>
+        <button onClick={handleClick}>Submit</button>
+      </div>
+      <div className={bloques}>
+        {
+          show?
+          <ShowAll json={responseData}/>
+          :null
+        }
+        {
+          show?
+          <ShowAll json={responseData1}/>
+          :null
+        }
+      </div>
     </div>
   )
 }
--- src/pages/LogIn/LogIn.jsx
+++ src/pages/LogIn/LogIn.jsx
@@ -1,19 +1,61 @@
 import React from 'react'
+import Axios from 'axios'
 import PropTypes from 'prop-types'
-import { useHistory, Link } from 'react-router-dom'
+import { useState } from 'react'
+import { Link, useHistory } from 'react-router-dom'
 
 import { styles } from './LogIn.module.css'
 
 const LogIn = () => {
+
+  const [correo, setCorreo] = useState("");
+  const [contraseña, setContraseña] = useState("");
+  const history = useHistory()
+
+  const handleChangeCorreo = (valor) => {
+    // 👇 Store the input value to local state
+    setCorreo(valor.target.value);
+  };
+
+  const handleChangeContraseña = (valor) => {
+    // 👇 Store the input value to local state
+    setContraseña(valor.target.value);
+  };
+
+  const getTipo = async() => {
+    try {
+      const response = await Axios.get(`http://localhost:3000/api/v1/usuarios/${correo}&${contraseña}`)
+      return response.data
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  const handleClick = async() => {
+    const jason = await getTipo()
+    var tipo
+
+    {jason.map((row, index) => {
+      tipo = row.tipo
+    })}
+
+    if (tipo == "administrador"){
+      history.push("/busquedamensual")
+    }
+    else {
+      history.push("/expediente")
+    }
+  }
+
   return (
     <div className={styles}>
       <h1>Log In</h1>
       <h2>Correo</h2>
-      <input type="text" placeholder="Escriba su correo"/>
+      <input type="text" placeholder="Escriba su correo" onChange={handleChangeCorreo} />
       <h2>Contraseña</h2>
-      <input type="text" placeholder="Escriba su contraseña"/>
+      <input type="text" placeholder="Escriba su contraseña" onChange={handleChangeContraseña}/>
       <br />
-      <input type="submit" value="INGRESAR"/>
+      <button onClick={handleClick}>Ingresar</button>
       <br />
       <Link to="/signin">Registrate</Link>
     </div>
